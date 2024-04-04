a0058cceecc80b8f2ec4d60ec4df94a17870e601
Andy Hattemer <andy@neon.tech>
2024-04-04 00:22:18 -0400

    add toc

--- app/page.tsx
+++ app/page.tsx
@@ -60,18 +60,38 @@ export default function Home() {
       <Analytics />
 
       {/* Content */}
-      <section className="w-full prose dark:prose-invert">
-        <div className="bg-primary/5 border-primary border-l-4 px-4 -mx-4 py-3 mb-12">
-          <div className="flex items-center gap-1">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-primary shrink-0 w-6 h-6"><path strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <span className="font-semibold text-primary">New to Neon?</span>
+      <section className="w-full pt-40">
+        <div className="sticky top-12 w-0 h-0 hidden xl:block">
+          <div className="w-48">
+            <div className="divider w-12"></div>
+            <div className="flex flex-col gap-4 font-medium text-base-content/80">
+              <a className="hover:text-base-content" href="#about-neon-cold-starts">About Neon Cold Starts</a>
+              <a className="hover:text-base-content" href="#cold-start-faqs" >Cold Start FAQs</a>
+              <a className="hover:text-base-content" href="#benchmark-methodology" >Benchmark methodology</a>
+              <a className="hover:text-base-content" href="#try-it-yourself" >Try It Yourself</a>
+            </div>
           </div>
-          <p className="m-0 mt-1">Neon is&nbsp;<a href="https://neon.tech/?ref=a">serverless Postgres</a>: Standard PostgreSQL in a cloud platform that separates storage and compute, unlocking features like branching, autoscaling, and&nbsp;scale to zero.</p>
         </div>
-        <Intro />
-        <Graphic version="IDE" />
-        <Details />
+        <div className="-mt-40 mx-auto prose dark:prose-invert">
+          <div className="bg-primary/5 border-primary border-l-4 px-4 -mx-4 py-3 mb-12">
+            <div className="flex items-center gap-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-primary shrink-0 w-6 h-6"><path strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+              <span className="font-semibold text-primary">New to Neon?</span>
+            </div>
+            <p className="m-0 mt-1">Neon is&nbsp;<a href="https://neon.tech/">serverless Postgres</a>: Standard PostgreSQL in a cloud platform that separates storage and compute, unlocking features like branching, autoscaling, and&nbsp;scale to zero.</p>
+          </div>
+          <Intro />
+          <Graphic version="IDE" />
+          <Details />
+        </div>
       </section>
+      <a href="https://neon.tech">
+        <svg className="fill-base-200 dark:fill-neutral w-24 h-24 hover:fill-primary transition-all" viewBox="-50 -50 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M0 51.7241C0 23.1577 23.1574 0 51.7234 0H248.273C276.839 0 299.996 23.1577 299.996 51.7241V218.891C299.996 248.445 262.598 261.271 244.46 237.938L187.756 164.994V253.448C187.756 279.158 166.914 300 141.205 300H51.7234C23.1574 300 0 276.842 0 248.276V51.7241ZM51.7234 41.3793C46.0102 41.3793 41.3788 46.0108 41.3788 51.7241V248.276C41.3788 253.989 46.0102 258.621 51.7234 258.621H142.757C145.613 258.621 146.377 256.305 146.377 253.448V134.831C146.377 105.277 183.775 92.4508 201.913 115.784L258.617 188.728V51.7241C258.617 46.0108 259.158 41.3793 253.445 41.3793H51.7234Z"/>
+          <path d="M248.277 0C276.843 0 300 23.1577 300 51.7241V218.891C300 248.445 262.602 261.271 244.464 237.938L187.76 164.994V253.448C187.76 279.158 166.918 300 141.209 300C144.066 300 146.381 297.684 146.381 294.828V134.831C146.381 105.277 183.779 92.4508 201.917 115.784L258.621 188.728V10.3448C258.621 4.63154 253.99 0 248.277 0Z" />
+        </svg>
+      </a>
+
     </main>
   );
 }
